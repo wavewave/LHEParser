685c7781007388f0245bb7156f8817ebba11bd19
Ian-Woo Kim <ianwookim@gmail.com>
2012-10-21 11:38:42 -0400

    travis-ci integration

---
+++ builddep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+sudo apt-get install libblas-dev
+sudo apt-get install liblapack-dev
+sudo apt-get install libgsl0-dev
+
+cabal install hmatrix 
+
+ghc-pkg list
+mkdir deps
+git clone https://github.com/wavewave/devadmin.git deps/devadmin
+cd deps/devadmin ; cabal install --force-reinstalls ; cd ../../
+$HOME/.cabal/bin/build cloneall --config=build.conf
+$HOME/.cabal/bin/build bootstrap --config=build.conf
+cabal install --enable-tests
+
